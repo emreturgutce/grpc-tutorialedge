adf7de295aca11231f1574d4097d76a9d0945a84
Emre Turgut <emrebursalee@gmail.com>
2021-05-24 17:52:41 +0300

    chat

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/emreturgutce/grpc-tutorialedge
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20210521195947-fe42d452be8f // indirect
+	golang.org/x/sys v0.0.0-20210521203332-0cec03c779c1 // indirect
+	google.golang.org/genproto v0.0.0-20210521181308-5ccab8a35a9a // indirect
+	google.golang.org/grpc v1.38.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
---
+++ server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/emreturgutce/grpc-tutorialedge/chat"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := chat.Server{}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChatServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
+}
